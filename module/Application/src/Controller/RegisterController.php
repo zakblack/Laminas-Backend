43ab8359787aaebf42fbf373615d68187623f830
ZakariaGHB <zakariagharib@gmail.com>
2020-02-10 15:34:18 +0100

    Update RegisterController.php

--- module/Application/src/Controller/RegisterController.php
+++ module/Application/src/Controller/RegisterController.php
@@ -13,7 +13,6 @@ namespace Application\Controller;
 
 use Application\Entity\User;
 use Doctrine\ORM\EntityManager;
-use Firebase\JWT\JWT;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\JsonModel;
 use Laminas\View\Model\ViewModel;
@@ -35,57 +34,13 @@ class RegisterController extends AbstractActionController
 
     // This is the default "index" action of the controller. It displays the
     // Posts page containing the recent blog posts.
-    public function indexAction()
-    {
 
-        // Get recent posts
-        $posts = $this->entityManager->getRepository(User::class)->findAll();
-        $joueurs=[];
-        foreach ($posts as $p ){
-            $joueur=array("id"=>$p->getId(),"username"=>$p->getUsername());
-            array_push($joueurs, $joueur);
-        }
 
-        return new JsonModel($joueurs);
-        /**return new JsonModel([
-            'status' => 'SUCCESS',
-            'message'=>'Here is your data',
-            'data' => [
-                'full_name' => 'John Doe',
-                'address' => '51 Middle st.'
-            ]
-        ]);**/
-    }
 
-    public function loginAction()
-    {
-        $username = $this->params()->fromPost("username");
-        $password =  $this->params()->fromPost("password");
-        $player = $this->entityManager->getRepository(User::class)->findOneBy(["username"=>$username,"password"=>$password]);
-        if (isset($player)) {
-            /**$key = "123456";
-            $payload = array(
-                "id" => $player->getId(),
-                "time" => time(),
-                "username" => $player->getUsername()
-            );
-            $token = JWT::encode($payload, $key);**/
 
-            $token = JwtController::generateJwt($player);
-            $this->getResponse()->setStatusCode(200);
-            return new JsonModel(["message"=>"success","token"=>$token]);
+    public function indexAction(){
 
-        }
-        else {
-            $this->getResponse()->setStatusCode(403);
-            return new JsonModel(["message"=>"Forbidden"]);
-        }
-
-    }
-
-    public function registerAction(){
-
-        $username = $this->params()->fromPost("username");
+       /** $username = $this->params()->fromPost("username");
         $password =  $this->params()->fromPost("password");
         $nom = $this->params()->fromPost("nom");
         $prenom = $this->params()->fromPost("prenom");
@@ -107,18 +62,13 @@ class RegisterController extends AbstractActionController
             $this->entityManager->persist($user);
             $this->entityManager->flush();
 
-            $this->getResponse()->setStatusCode(200);
-            return new JsonModel(["message"=>"success","id"=>$user->getId()]);
+            $this->getResponse()->setStatusCode(200);**/
+            return new JsonModel(["message"=>"success"]);
 
 
 
     }
 
-    public function verifyAction(){
-        $msg = $this->getRequest()->getHeaders();
 
-        return new JsonModel([var_dump($msg->get('authorization')->value)]);
-        //return new JsonModel([$msg->authorization()]);
-    }
 
 }
