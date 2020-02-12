0003d85556078a0fbc60f42ac4ce821b3641ba97
ZakariaGHB <zakariagharib@gmail.com>
2020-02-12 22:34:44 +0100

    Always making Moussa happy like always

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -2,5 +2,17 @@
 use Doctrine\DBAL\Driver\PDOMySql\Driver as PDOMySqlDriver;
 
 return [
-
+    'doctrine' => [
+        'connection' => [
+            'orm_default' => [
+                'driverClass' => PDOMySqlDriver::class,
+                'params' => [
+                    'host'     => 'b54kr7o4sssj7dm1qdld-mysql.services.clever-cloud.com',
+                    'user'     => 'uaqruimhcy6k4jnk',
+                    'password' => 'mbsIaOPQovWMmiR06kFR',
+                    'dbname'   => 'b54kr7o4sssj7dm1qdld',
+                ]
+            ],
+        ],
+    ],
 ];
