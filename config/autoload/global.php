54220666ac304fd9809ad9ac2a7e51e2ba1f1d75
ZakariaGHB <zakariagharib@gmail.com>
2020-02-12 22:34:26 +0100

    Always making Moussa happy like always

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -2,17 +2,5 @@
 use Doctrine\DBAL\Driver\PDOMySql\Driver as PDOMySqlDriver;
 
 return [
-    'doctrine' => [
-        'connection' => [
-            'orm_default' => [
-                'driverClass' => PDOMySqlDriver::class,
-                'params' => [
-                    'host'     => 'localhost',
-                    'user'     => 'root',
-                    'password' => '',
-                    'dbname'   => 'project',
-                ]
-            ],
-        ],
-    ],
+
 ];
